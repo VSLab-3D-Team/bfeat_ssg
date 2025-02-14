551573121ea4cb81a12e53232ae5f9cc340c030e
enopid <58299653+enopid@users.noreply.github.com>
2025-02-14 22:36:45 +0900

    change compute_predicate_acc_per_class and add draw_graph
    wandb의 그래프는 빈도와 정확도를 동시에 못 나타내
    wandb에 띄우는 방식을 pyplot을 통해 그린 이미지를 띄우는 방식으로 변경

--- runners/base_trainer.py
+++ runners/base_trainer.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import wandb
+import matplotlib.pyplot as plt
 import clip
 from datetime import datetime
 import os
@@ -190,6 +191,28 @@ class BaseTrainer(ABC):
 
         return predicate_mean_1 * 100, predicate_mean_3 * 100, predicate_mean_5 * 100
     
+    def draw_graph(self,predicate_mean, topk_index):
+        fig, ax1 = plt.subplots(figsize=(13,10))
+
+        ax1.set_xlabel("Class")
+        ax1.set_ylabel("Frequency", color="blue")
+        ax1.plot([predicate_mean[i][0] for i in range(len(predicate_mean))], [predicate_mean[i][1] for i in range(len(predicate_mean))], marker="o", linestyle="-", color="blue", label="Frequency")
+        ax1.tick_params(axis="y", labelcolor="blue")
+        ax1.tick_params(axis="x", rotation=90)
+        
+        ax2 = ax1.twinx()
+        ax2.set_ylabel("Acc", color="orange")
+        ax2.bar([predicate_mean[i][0] for i in range(len(predicate_mean))], [predicate_mean[i][2][topk_index] for i in range(len(predicate_mean))], alpha=0.6, color="orange", label="Acc")
+        ax2.tick_params(axis="y", labelcolor="orange")
+        
+        fig.tight_layout()
+        # 그래프 제목
+        tmp=[1,3,5]
+        fig.suptitle(f"rel_acc_per_cls@{tmp[topk_index]}")
+        plt.subplots_adjust(top=0.85)
+
+        return fig
+    
     def compute_predicate_acc_per_class(self, cls_matrix_list, topk_pred_list):
         cls_dict = {}
         for i in range(26):
@@ -202,7 +225,7 @@ class BaseTrainer(ABC):
                 total_cnt+=1
         
         predicate_mean = []
-        acc_freq=1
+        acc_freq=total_cnt
         for i in range(26):
             l = len(cls_dict[i])
             if l > 0:
@@ -211,23 +234,21 @@ class BaseTrainer(ABC):
                 m_5 = (np.array(cls_dict[i]) <= 5).sum() / len(cls_dict[i])
                 
                 cls=self.rel_label_list[i]
-                freq=len(cls_dict[i])/total_cnt
-                predicate_mean.append([cls,acc_freq,[m_1,m_3,m_5]])
-                acc_freq-=freq
+                freq=len(cls_dict[i])
+                predicate_mean.append([cls,freq,[m_1,m_3,m_5]])
         predicate_mean.sort(key=lambda x: x[1],reverse=True)
-
-        table1 = wandb.Table(columns=["Class", "Frequency", "mAcc@1"])
-        table2 = wandb.Table(columns=["Class", "Frequency", "mAcc@3"])
-        table3 = wandb.Table(columns=["Class", "Frequency", "mAcc@5"])
-
-        for cls, freq, m_list in predicate_mean:
-            table1.add_data(cls, freq, m_list[0])
-            table2.add_data(cls, freq, m_list[1])
-            table3.add_data(cls, freq, m_list[2])
+        for i in range(len(predicate_mean)):
+            freq=predicate_mean[i][1]
+            predicate_mean[i][1]=acc_freq/total_cnt
+            acc_freq-=freq
         
-        self.wandb_log["Validation/Acc@1/rel_acc_per_cls"]=table1
-        self.wandb_log["Validation/Acc@3/rel_acc_per_cls"]=table2
-        self.wandb_log["Validation/Acc@5/rel_acc_per_cls"]=table3
+        fig1=self.draw_graph(predicate_mean,0)
+        fig2=self.draw_graph(predicate_mean,1)
+        fig3=self.draw_graph(predicate_mean,2)
+        self.wandb_log["rel_acc_per_cls"]=[wandb.Image(fig1),wandb.Image(fig2),wandb.Image(fig3)]
+        plt.close(fig1)
+        plt.close(fig2)
+        plt.close(fig3)
         
     def save_checkpoint(self, exp_name, file_name):
         save_file = os.path.join(f'checkpoints/{exp_name}/models/', file_name)
